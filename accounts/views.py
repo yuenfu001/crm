912f15669e3253b9a078d55df6b3760baba67ecb
yuenfu001 <64754712+yuenfu001@users.noreply.github.com>
2020-09-06 23:30:57 +0100

    added views and urls in accounts app

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home(request):
+    return HttpResponse("you are in the index page")
+
+def contact(request):
+    return HttpResponse("contact page")
