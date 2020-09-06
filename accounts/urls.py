89472b7f147ff1b57ebfccf160053d465e71019a
yuenfu001 <64754712+yuenfu001@users.noreply.github.com>
2020-09-06 23:42:09 +0100

    added stuffs in the urls.py files

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home, contact
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.home, name="home"),
+    path("contact/", views.contact, name="contact"),
 ]
